cca1e2bc1f00723cf703727e4732ad1c74d95015
Saikat Mandal <saikat@Saikats-Mac-mini.local>
2026-06-11 22:03:51 +0530

    added docker file

---
+++ billing-service/src/main/java/org/pm/billingservice/grpc/BillingGrpcService.java
@@ -0,0 +1,8 @@
+package org.pm.billingservice.grpc;
+
+import billing.BillingServiceGrpc;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class BillingGrpcService extends BillingServiceGrpc.BillingServiceImplBase {
+}
